547b12d3c8fb48b3df72082f7a069f95b4cb07ed
WildLinx21 <kurg99@mail.ru>
2018-04-16 11:43:27 +1000

    Заделана 1 часть

--- Project1012/Project1012.cpp
+++ Project1012/Project1012.cpp
@@ -1,13 +1,24 @@
-﻿// Project1012.cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
-
-
+﻿#include "stdafx.h"
+#include <iostream>
+#include <studio.h>
+#include "locale"
+using namespace std;
 int main()
 {
-	int u=0;
-	int i = 0;
+	string NAME;
+	cin >> NAME;
+	system("cls");
+	setlocale(LC_ALL, "Russian");
+	FILE*infile;
+	infile = fopen(NAME.c_str()); // УЗНАТЬ
+	if (!infile)
+	{
+		cout << "Файл не найден!" << endl;
+		return 1;
+	}
+
+
+
 	return 0;
 }
 
